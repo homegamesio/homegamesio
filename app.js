d27a7f0fd1009142eef19f6ef287d229375dbdbe
Joseph Garcia <prosif@gmail.com>
2019-11-10 12:43:19 -0800

    hot load index.html, add basic divs

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const http = require('http');
 const fs = require('fs');
-const path = require('path');
-
-const index = fs.readFileSync('index.html');
 
 const server = http.createServer((req, res) => {
+    const index = fs.readFileSync('index.html');
     res.setHeader('Content-Type', 'text/html');
     res.end(index);
 });
