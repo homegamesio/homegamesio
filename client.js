29a932339e4ccb266e249a6fe1f638ff519d8915
Joseph Garcia <prosif@gmail.com>
2023-07-15 15:46:43 -0700

    for real for real for real

--- client.js
+++ client.js
@@ -347,7 +347,6 @@ const modals = {
             const _loader = loader();
             // container.appendChild(_loader);
 
-            console.log('path ' + path);
             // makeGet(`https://builds.homegames.io${path}`).then((_buildInfo) => {
                 // container.removeChild(_loader);
                 // const buildInfo = JSON.parse(_buildInfo);
@@ -367,6 +366,15 @@ const modals = {
 
                 container.appendChild(downloadHeader);
 
+                const setupGuide = document.createElement('div');
+                const setupGuideLink = document.createElement('a');
+                setupGuideLink.href = 'https://youtu.be/UdW-FH3QQhA';
+                setupGuideLink.target = '_blank';
+                setupGuideLink.innerHTML = 'Setup Guide';
+                setupGuide.style = 'text-align: center; color: white;';
+                setupGuideLink.style = 'color: white; text-decoration: none'
+
+                setupGuide.appendChild(setupGuideLink);
 
                 const latestDiv = document.createElement('div');
                 latestDiv.style = 'margin-top: 48px';
@@ -374,26 +382,32 @@ const modals = {
                 const stableDiv = document.createElement('div');
 
                 const latestWindowsArm = document.createElement('a');
+                latestWindowsArm.className = 'downloadLink';
                 latestWindowsArm.innerHTML = 'Windows (ARM)';
                 latestWindowsArm.href = 'https://builds.homegames.io/latest/homegames-win-arm64.exe';
 
                 const latestWindows = document.createElement('a');
+                latestWindows.className = 'downloadLink';
                 latestWindows.innerHTML = 'Windows (x86)';
                 latestWindows.href = 'https://builds.homegames.io/latest/homegames-win-x64.exe';
 
                 const latestLinux = document.createElement('a');
+                latestLinux.className = 'downloadLink';
                 latestLinux.innerHTML = 'Linux (x86)';
                 latestLinux.href = 'https://builds.homegames.io/latest/homegames-linux-x64';
 
                 const latestLinuxArm = document.createElement('a');
+                latestLinuxArm.className = 'downloadLink';
                 latestLinuxArm.innerHTML = 'Linux (ARM)';
                 latestLinuxArm.href = 'https://builds.homegames.io/latest/homegames-linux-arm64';
 
                 const latestMac = document.createElement('a');
+                latestMac.className = 'downloadLink';
                 latestMac.innerHTML = 'macOS (x86)';
                 latestMac.href = 'https://builds.homegames.io/latest/hg-mac-x64.zip';
 
                 const latestMacArm = document.createElement('a');
+                latestMacArm.className = 'downloadLink';
                 latestMacArm.innerHTML = 'macOS (ARM)';
                 latestMacArm.href = 'https://builds.homegames.io/latest/hg-mac-arm64.zip';
 
@@ -413,30 +427,37 @@ const modals = {
                 latestDiv.appendChild(latestLinuxArm);
 
                 const stableWindows = document.createElement('a');
+                stableWindows.className = 'downloadLink';
                 stableWindows.innerHTML = 'Windows (x86)';
                 stableWindows.href = 'https://builds.homegames.io/stable/homegames-win-x64.exe';
 
                 const stableLinux = document.createElement('a');
+                stableLinux.className = 'downloadLink';
                 stableLinux.innerHTML = 'Linux (x86)';
                 stableLinux.href = 'https://builds.homegames.io/stable/homegames-linux-x64';
 
                 const stableMac = document.createElement('a');
+                stableMac.className = 'downloadLink';
                 stableMac.innerHTML = 'macOS (x86)';
                 stableMac.href = 'https://builds.homegames.io/stable/hg-mac-x64.zip';
 
                 const stableWindowsArm = document.createElement('a');
+                stableWindowsArm.className = 'downloadLink';
                 stableWindowsArm.innerHTML = 'Windows (ARM)';
                 stableWindowsArm.href = 'https://builds.homegames.io/stable/homegames-win-arm64.exe';
 
                 const stableLinuxArm = document.createElement('a');
+                stableLinuxArm.className = 'downloadLink';
                 stableLinuxArm.innerHTML = 'Linux (ARM)';
                 stableLinuxArm.href = 'https://builds.homegames.io/stable/homegames-linux-arm64';
 
                 const stableMacArm = document.createElement('a');
+                stableMacArm.className = 'downloadLink';
                 stableMacArm.innerHTML = 'macOS (ARM)';
                 stableMacArm.href = 'https://builds.homegames.io/stable/hg-mac-arm64.zip';
 
                 const stableHeader = document.createElement('h3');
+                latestWindows.className = 'downloadLink';
                 stableHeader.innerHTML = 'Stable';
                 stableHeader.style = "text-align: center";
 
@@ -497,6 +518,7 @@ const modals = {
                 // instructions.innerHTML = 'Run the homegames server package and navigate to homegames.link in any browser on your local network to play games';
                 // instructions.style = 'text-align: center;';
 
+                container.appendChild(setupGuide);
                 container.appendChild(stableDiv);
                 container.appendChild(latestDiv);
             // });
